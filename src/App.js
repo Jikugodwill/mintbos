b89b4ac84409b86b77f83d8dbcad3784f7315f8e
Ebube Agwaze <ebubeagwaze@gmail.com>
2023-11-07 13:27:17 +0100

    finished making changes

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ import Big from "big.js";
 import { NetworkId, Widgets } from "./data/widgets";
 import { useEthersProviderContext } from "./data/web3";
 import SignInPage from "./pages/SignInPage";
+import { NavigationWrapper } from "./components/navigation/NavigationWrapper";
 
 export const refreshAllowanceObj = {};
 const documentationHref = "https://social.near-docs.io/";
@@ -166,15 +167,18 @@ function App(props) {
         <Router basename={process.env.PUBLIC_URL}>
           <Switch>
             <Route path={"/signin"}>
+              <NavigationWrapper {...passProps} />
               <SignInPage {...passProps} />
             </Route>
             <Route path={"/embed/:widgetSrc*"}>
               <EmbedPage {...passProps} />
             </Route>
             <Route path={"/edit/:widgetSrc*"}>
+              <NavigationWrapper {...passProps} />
               <EditorPage {...passProps} />
             </Route>
             <Route path={"/:widgetSrc*"}>
+              <NavigationWrapper {...passProps} />
               <ViewPage {...passProps} />
             </Route>
           </Switch>
