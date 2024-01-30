2b95cad67e3c2a734e6ad452abf19041b3bd97f9
Jiku Godwill Nsanwi <65053264+Jikugodwill@users.noreply.github.com>
2024-01-30 07:58:48 +0100

    Update profilePage widget path

--- src/data/widgets.js
+++ src/data/widgets.js
@@ -26,7 +26,7 @@ const MainnetWidgets = {
   widgetMetadata: "mob.near/widget/WidgetMetadata",
   profileImage: "mob.near/widget/ProfileImage",
   notificationButton: "mob.near/widget/NotificationButton",
-  profilePage: "bos.genadrop.near/widget/DropFlow.ArtistPage",
+  profilePage: "bos.genadrop.near/widget/DropFlow.ArtistPage.Index",
   profileName: "patrick.near/widget/ProfileName",
   editorComponentSearch: "mob.near/widget/Editor.ComponentSearch",
   profileInlineBlock: "mob.near/widget/Profile.InlineBlock",
